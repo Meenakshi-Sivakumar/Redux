9a5f2db9c4ad264eb6e04fc24b6e47e3d6761b47
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-04-20 16:22:02 +0530

    Create action-creator.js

---
+++ action-creator.js
@@ -0,0 +1,8 @@
+//An action creator is simply a JavaScript function that returns an action. 
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+function actionCreator(){
+  return action;
+}
