1297290fad185bc857a3e60da7c66811fc7ffb49
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-04-20 16:24:42 +0530

    Create handle-multiple-action.js

---
+++ handle-multiple-action.js
@@ -0,0 +1,37 @@
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // here
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        authenticated: true
+      };
+
+    case "LOGOUT":
+      return {
+        authenticated: false
+      };
+
+    default:
+      return defaultState;
+  }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
+store.dispatch(logoutUser());
+console.log(store.getState())
