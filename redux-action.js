e8fbe1b7b38b1c1e5545f6854d244fd2a33c0ff6
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-04-20 16:21:23 +0530

    Create redux-action.js

---
+++ redux-action.js
@@ -0,0 +1,4 @@
+// Writing a Redux action is as simple as declaring an object with a type property. Declare an object action and give it a property type set to the string 'LOGIN'.
+const action = {
+  type: 'LOGIN'
+}
